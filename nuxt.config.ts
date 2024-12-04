8b9b59ea8e3df4d3bc00b17055d168b8c2cd57ac
Deimos308 <andrew.saienko.308@gmail.com>
2024-12-05 01:27:00 +0200

    chore: Update npm dependencies and configuration files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  extends: ["shadcn-docs-nuxt", "@nuxthq/studio"],
+  extends: ["shadcn-docs-nuxt"],
+  modules: ["@nuxthq/studio"],
   routeRules: {
     "/": { prerender: true },
   },
+
   compatibilityDate: "2024-07-06",
 });
