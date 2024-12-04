ea2133430ee47695ccf8d1f9302f096c04eb1f73
sai308 <164794812+sai308@users.noreply.github.com>
2024-12-04 23:00:53 +0000

    Deploying to gh-pages from @ sai308/edu-blog@f012b0fab52cb7f37c400672e895271400a4d829 🚀

--- nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true },
-  extends: ["shadcn-docs-nuxt"],
-  routeRules: {
-    "/": { prerender: true },
-  },
-  compatibilityDate: "2024-07-06",
-});
